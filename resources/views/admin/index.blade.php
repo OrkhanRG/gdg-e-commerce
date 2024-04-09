1f725fb7842ee54e2fff3eb1ad9610d008e1b036
OrkhaN <orxanismayilov851@gmail.com>
2024-04-10 00:00:07 +0400

    admin panel parçalandı

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+@section('title', 'Dashboard Səhifəsi')
+
+@push('css')
+@endpush
+
+@section('content')
+    <h5>Dashboard Page</h5>
+@endsection
+
+@push('js')
+@endpush
